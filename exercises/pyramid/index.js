7d2d52d5ac5bd72383c2e523dcf2af7f54151abb
kwanwooi25 <kwanwoo.jeong@gmail.com>
2018-10-08 13:19:42 +0900

    pyramid solved

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// solution #2
+function pyramid(n, row = 0, level = '') {
+  if (n === row) return;
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add = ' ';
+  if (midpoint - row <= level.length && level.length <= midpoint + row) {
+    add = '#';
+  }
+
+  pyramid(n, row, level + add);
+}
+
+// // my solution
+// function pyramid(n) {
+//   const totalCols = n * 2 - 1;
+
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let col = 1; col <= totalCols; col++) {
+//       if ((n - row) <= col && col <= (n + row)) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
 
 module.exports = pyramid;
